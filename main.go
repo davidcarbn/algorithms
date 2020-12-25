8ce2bab8ed6b956e1ade7cd447078defbe32a46f
davidcarbn <davidcarbon@web.de>
2020-12-25 17:07:24 +0100

    Create Graph and Vertices with constructor of the structs

--- main.go
+++ main.go
@@ -1,12 +1,25 @@
 package main
 
 import (
-	"fmt"
-
 	"github.com/davidcarbn/algorithms/data-structures/graph"
+	"github.com/davidcarbn/algorithms/data-structures/graph/vertex"
 )
 
 func main() {
-	fmt.Println("Test")
-	var graph graph.Graph
+	g := graph.New()
+
+	v1 := vertex.New(1)
+	v2 := vertex.New(2)
+	v3 := vertex.New(3)
+	v4 := vertex.New(4)
+
+	g.AddVertex(v1)
+	g.AddVertex(v2)
+	g.AddVertex(v3)
+	g.AddVertex(v4)
+
+	g.AddEdge(v1, v2)
+	g.AddEdge(v1, v3)
+	g.AddEdge(v2, v3)
+	g.AddEdge(v2, v4)
 }
