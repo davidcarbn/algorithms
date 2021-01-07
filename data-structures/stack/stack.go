a6d14d5bfc211c60b4f9416afe9f7f1ccd3bbea8
davidcarbn <davidcarbon@web.de>
2021-01-07 17:07:01 +0100

    Stack functions changed to answer with Error if stack is empty

--- data-structures/stack/stack.go
+++ data-structures/stack/stack.go
@@ -1,20 +1,38 @@
 package stack
 
+import (
+	"errors"
+
+	"github.com/davidcarbn/algorithms/data-structures/graph/vertex"
+)
+
+type Vertex = vertex.Vertex
 type Stack struct {
-	Elems []int
+	Elems []Vertex
 }
 
-func (s *Stack) New() *Stack {
+func New() *Stack {
 	return &Stack{}
 }
-func (s *Stack) Push(value int) {
-	s.Elems = append(s.Elems,value)
+func (s *Stack) IsEmpty() bool {
+	return len(s.Elems) == 0
 }
-func (s *Stack) Peek() int {
-	return s.Elems[len(s.Elems)-1]
+func (s *Stack) Push(value Vertex) {
+	s.Elems = append(s.Elems, value)
 }
-func (s *Stack) Pop() int {
-	top := s:Elems[len(s.Elems)-1]
+func (s *Stack) Peek() (Vertex, error) {
+	var top Vertex
+	if s.IsEmpty() {
+		return top, errors.New("Stack.Pop() Error: Stack is Empty")
+	}
+	return s.Elems[len(s.Elems)-1], nil
+}
+func (s *Stack) Pop() (Vertex, error) {
+	var top Vertex
+	if s.IsEmpty() {
+		return top, errors.New("Stack.Pop() Error: Stack is Empty")
+	}
+	top = s.Elems[len(s.Elems)-1]
 	s.Elems = s.Elems[:len(s.Elems)-1]
-	return top
+	return top, nil
 }
